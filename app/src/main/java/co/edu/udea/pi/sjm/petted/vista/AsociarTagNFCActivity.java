031edabd8975e3730ed5186e0ae33ebacda0c6c6
Juan <tinez7g@gmail.com>
2015-09-27 13:16:43 -0500

    Verificación de compatibilidad NFC.

--- app/src/main/java/co/edu/udea/pi/sjm/petted/vista/AsociarTagNFCActivity.java
+++ app/src/main/java/co/edu/udea/pi/sjm/petted/vista/AsociarTagNFCActivity.java
@@ -1,29 +1,53 @@
 package co.edu.udea.pi.sjm.petted.vista;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import co.edu.udea.pi.sjm.petted.R;
 
 public class AsociarTagNFCActivity extends AppCompatActivity {
 
     Button btnAsociar;
+    public static final String TAG = "NfcDemo";
+
+    private TextView tvIdTag;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asociar_tag_nfc);
-        btnAsociar = (Button)findViewById(R.id.btnAsociar);
+        btnAsociar = (Button) findViewById(R.id.btnAsociar);
         btnAsociar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        tvIdTag = (TextView) findViewById(R.id.tvIdTagNFC);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            tvIdTag.setText("NFC is disabled.");
+        } else {
+            tvIdTag.setText(NfcAdapter.EXTRA_ID);
+        }
     }
 
     @Override
