b90be865be95d424cc68e2a866e1eff358edb8ec
Juan <tinez7g@gmail.com>
2015-10-04 21:34:47 -0500

    Manejo de foto: seleccionar de galeria o tomar foto, funcional almacena la foto en la bd sqlite local.

--- app/src/main/java/co/edu/udea/pi/sjm/petted/vista/mascota_nueva/MascotaNuevaActivity.java
+++ app/src/main/java/co/edu/udea/pi/sjm/petted/vista/mascota_nueva/MascotaNuevaActivity.java
@@ -1,9 +1,18 @@
 package co.edu.udea.pi.sjm.petted.vista.mascota_nueva;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
@@ -17,9 +26,14 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -43,6 +57,17 @@ public class MascotaNuevaActivity extends AppCompatActivity {
     private Spinner spinnerGenero;
     private EditText etNombreMascota;
     private ImageButton ibFechaNacimiento;
+    private Button btnFoto;
+    private ImageView ivFotoPrevia;
+    private Bitmap foto;
+
+    private String APP_DIRECTORY = "myPictureApp/";
+    private String MEDIA_DIRECTORY = APP_DIRECTORY + "media";
+    private String TEMPORAL_PICTURE_NAME = "temporal.jpg";
+
+
+    private final int PHOTO_CODE = 100;
+    private final int SELECT_PICTURE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +91,9 @@ public class MascotaNuevaActivity extends AppCompatActivity {
         spinnerRazaMascota = (Spinner) findViewById(R.id.spinnerRazaMascota);
         spinnerGenero = (Spinner) findViewById(R.id.spinnerGenero);
         etNombreMascota = (EditText) findViewById(R.id.etNombreMascota);
-        ibFechaNacimiento = (ImageButton)findViewById(R.id.ibFechaNacimiento);
+        ibFechaNacimiento = (ImageButton) findViewById(R.id.ibFechaNacimiento);
+        btnFoto = (Button) findViewById(R.id.btnFoto);
+        ivFotoPrevia = (ImageView) findViewById(R.id.ivFotoPrevia);
 
 
         formateadorDeFecha = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
@@ -78,6 +105,76 @@ public class MascotaNuevaActivity extends AppCompatActivity {
 
         inicializarSpinners();
 
+    }
+
+    public void onClickFoto(View view) {
+        final CharSequence[] opciones = {"Tomar Foto", "Seleccionar foto", "Cancelar"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MascotaNuevaActivity.this);
+        builder.setTitle("Selecciona una opción: ");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int seleccion) {
+                switch (seleccion) {
+                    case 0:
+                        tomarFoto();
+                        break;
+                    case 1:
+                        seleccionarFoto();
+                        break;
+                    case 2:
+                        dialog.dismiss();
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case PHOTO_CODE:
+                if (resultCode == RESULT_OK) {
+                    String dir = Environment.getExternalStorageDirectory() + File.separator + MEDIA_DIRECTORY
+                            + File.separator + TEMPORAL_PICTURE_NAME;
+                    foto = BitmapFactory.decodeFile(dir);
+                    ivFotoPrevia.setImageBitmap(foto);
+                }
+                break;
+            case SELECT_PICTURE:
+                if (resultCode == RESULT_OK) {
+                    Uri dir = data.getData();
+                    Bitmap fotoPrevia;
+                    try {
+                        foto = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dir);
+                        ivFotoPrevia.setImageBitmap(foto);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }
+    }
+
+    public void tomarFoto() {
+        File file = new File(Environment.getExternalStorageDirectory(), MEDIA_DIRECTORY);
+        file.mkdirs();
+
+        String path = Environment.getExternalStorageDirectory() + File.separator + MEDIA_DIRECTORY
+                + File.separator + TEMPORAL_PICTURE_NAME;
+        File newFile = new File(path);
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
+
+        startActivityForResult(i, PHOTO_CODE);
+    }
+
+    public void seleccionarFoto() {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        i.setType("image/*");
+        startActivityForResult(Intent.createChooser(i, "Selecciona app de imagen"), SELECT_PICTURE);
     }
 
     @Deprecated
@@ -110,8 +207,11 @@ public class MascotaNuevaActivity extends AppCompatActivity {
         m = new Mascota();
         m.setNombre(etNombreMascota.getText().toString());
         m.setPropietario(u);
-        m.setFoto(Utility.resizeImage(this, R.drawable.mascota1, 300, 300));
-
+        if (foto != null) {
+            m.setFoto(Utility.resizeImage(foto, 300, 300));
+        } else {
+            m.setFoto(Utility.resizeImage(this, R.drawable.mascota1, 300, 300));
+        }
         switch (Validacion.validarMascota(m)) {
             case 0:
                 dao.insertarMascota(m, this);
